556c1c2153b3ba1f305f2e16cda61273988503e3
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-02 07:11:50 +0900

    Add the scroll view.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -9,19 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Background()
+        ScrollView {
             VStack {
-                Title(title: "Learn to code by watching others", subtitle: "See how experienced developers solve problems in real-time. Watching scripted tutorials is great. But understanding how developers think is invaluable.")
-                    .padding(.horizontal, 20)
-                Spacer()
-                    .frame(height: 50)
-                PromotionCard(tryPeriod: 7, monthlyFee: 20)
-                Spacer()
-                    .frame(height: 30)
-                TrialForm()
+                ZStack {
+                    Background()
+                    VStack {
+                        Title(title: "Learn to code by watching others", subtitle: "See how experienced developers solve problems in real-time. Watching scripted tutorials is great. But understanding how developers think is invaluable.")
+                            .padding(.horizontal, 20)
+                        Spacer()
+                            .frame(height: 50)
+                        PromotionCard(tryPeriod: 7, monthlyFee: 20)
+                        Spacer()
+                            .frame(height: 30)
+                        TrialForm()
+                    }
+                }
             }
         }
+        .edgesIgnoringSafeArea(.all)
+        .background(Color("BackgroundColor"))
     }
 }
 
