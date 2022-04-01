982abd59a5d9b9a078525d7e131347fcf9dd6801
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-01 14:21:41 +0900

    Initial Commit

---
+++ Shared/IntroComponentWithSignupFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroComponentWithSignupFormApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/04/01.
+//
+
+import SwiftUI
+
+@main
+struct IntroComponentWithSignupFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
