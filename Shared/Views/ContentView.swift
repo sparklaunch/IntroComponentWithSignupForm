964d834fa582dc87042d9b3c5ffb934dfe3fbbca
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-04-01 14:38:26 +0900

    Add the title.

--- Shared/Views/ContentView.swift
+++ Shared/Views/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Background()
+            VStack {
+                Title(title: "Learn to code by watching others", subtitle: "See how experienced developers solve problems in real-time. Watching scripted tutorials is great. But understanding how developers think is invaluable.")
+                    .padding(.horizontal, 20)
+            }
         }
     }
 }
---
+++ Shared/Views/Title.swift
@@ -0,0 +1,37 @@
+//
+//  Title.swift
+//  IntroComponentWithSignupForm (iOS)
+//
+//  Created by Jinwook Kim on 2022/04/01.
+//
+
+import SwiftUI
+
+struct Title: View {
+    let title: String
+    let subtitle: String
+    var body: some View {
+        VStack(spacing: 15) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(.white)
+                .fixedSize(horizontal: false, vertical: true)
+            Text(subtitle)
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .lineSpacing(3)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct Title_Previews: PreviewProvider {
+    static var previews: some View {
+        Title(title: "Learn to code by watching others", subtitle: "See how experienced developers solve problems in real-time. Watching scripted tutorials is great. But understanding how developers think is invaluable.")
+            .padding()
+            .background(Color("BackgroundColor"))
+            .previewLayout(.sizeThatFits)
+    }
+}
